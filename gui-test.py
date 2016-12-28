b7ad24dfb823efa0322dc1e48bbc9dc6bff59225
Mike Hansen <mike.hansen@chemeng.utah.edu>
2016-12-27 22:47:53 -0700

    - more gui updates

--- gui-test.py
+++ gui-test.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import numpy as np
 import copy as copy
 import crocodile as cc
+import tkinter.font as font
 
 
 class Incrementer:
@@ -32,7 +33,10 @@ class Decrementer:
         return self.list
 
 
-class App:
+# add number of cards checker
+# improve geometry
+
+class Crocodile:
     def __init__(self):
         self.root = tk.Tk()
 
@@ -42,32 +46,32 @@ class App:
         frame = tk.Frame(self.root)
         frame.grid()
 
-        self.quitbutton = tk.Button(frame, text='quit', command=frame.quit)
+        self.quitbutton = tk.Button(frame, text='quit', command=frame.quit, font="Osaka")
         self.quitbutton.grid(row=0, column=0)
 
-        self.sethandbutton = tk.Button(frame, text='score hand', command=self.set_hand)
+        self.sethandbutton = tk.Button(frame, text='score hand', command=self.set_hand, font="Osaka")
         self.sethandbutton.grid(row=16, column=1)
-        self.handlabel = tk.Label(frame, text='')
+        self.handlabel = tk.Label(frame, text='', font="Osaka")
         self.handlabel.grid(row=16, column=2)
 
-        self.resethandbutton = tk.Button(frame, text='reset hand', command=self.reset_hand)
+        self.resethandbutton = tk.Button(frame, text='reset hand', command=self.reset_hand, font="Osaka")
         self.resethandbutton.grid(row=17, column=1)
 
-        self.scoredesclabel = tk.Label(frame, text='', justify=tk.LEFT)
+        self.scoredesclabel = tk.Label(frame, text='', justify=tk.LEFT, font="Osaka")
         self.scoredesclabel.grid(row=19, column=4, sticky=tk.W)
 
-        self.cribbutton = tk.Button(frame, text='test crib', command=self.test_crib)
+        self.cribbutton = tk.Button(frame, text='test crib', command=self.test_crib, font="Osaka")
         self.cribbutton.grid(row=18, column=1)
-        self.clearcribbutton = tk.Button(frame, text='clear test crib', command=self.clear_test_crib)
+        self.clearcribbutton = tk.Button(frame, text='clear test crib', command=self.clear_test_crib, font="Osaka")
         self.clearcribbutton.grid(row=18, column=3)
-        self.cribdesclabel = tk.Label(frame, text='', justify=tk.LEFT)
+        self.cribdesclabel = tk.Label(frame, text='', justify=tk.LEFT, font="Osaka")
         self.cribdesclabel.grid(row=18, column=4, sticky=tk.W)
-        self.ncribtext = tk.Entry(frame)
+        self.ncribtext = tk.Entry(frame, font="Osaka")
         self.ncribtext.grid(row=18, column=2)
 
-        self.starterlabel = tk.Label(frame, text='starter', justify=tk.LEFT)
+        self.starterlabel = tk.Label(frame, text='starter', justify=tk.LEFT, font="Osaka")
         self.starterlabel.grid(row=15, column=3, sticky=tk.W)
-        self.startertext = tk.Entry(frame)
+        self.startertext = tk.Entry(frame, font="Osaka")
         self.startertext.grid(row=16, column=3, sticky=tk.W)
 
         self.numeachcard = []
@@ -81,22 +85,22 @@ class App:
         for i in np.arange(0, 13):
             self.numeachcard.append(0)
 
-            label = tk.Label(frame, text=str(self.numeachcard[i]), justify=tk.LEFT)
+            label = tk.Label(frame, text=str(self.numeachcard[i]), justify=tk.LEFT, font="Osaka")
             self.cardlabels.append(label)
             self.cardlabels[i].grid(row=i, column=4, sticky=tk.W)
 
             self.incrementers.append(Incrementer(i, self.numeachcard, self.cardlabels[i]))
             self.decrementers.append(Decrementer(i, self.numeachcard, self.cardlabels[i]))
 
-            label = tk.Label(frame, text=str(cc.cardify(i+1)))
+            label = tk.Label(frame, text=str(cc.cardify(i+1)), font="Osaka")
             self.cardnames.append(label)
             self.cardnames[i].grid(row=i, column=2)
 
-            item = tk.Button(frame, text='+', command=self.incrementers[i])
+            item = tk.Button(frame, text='+', command=self.incrementers[i], font="Osaka")
             self.cardbuttonsplus.append(item)
             self.cardbuttonsplus[i].grid(row=i, column=1)
 
-            item = tk.Button(frame, text='-', command=self.decrementers[i])
+            item = tk.Button(frame, text='-', command=self.decrementers[i], font="Osaka")
             self.cardbuttonsminus.append(item)
             self.cardbuttonsminus[i].grid(row=i, column=3, sticky=tk.W)
 
@@ -136,6 +140,6 @@ class App:
     def clear_test_crib(self):
         self.cribdesclabel['text'] = ''
 
-app = App()
-app.root.mainloop()
-app.root.destroy()
+gustav = Crocodile()
+gustav.root.mainloop()
+gustav.root.destroy()
